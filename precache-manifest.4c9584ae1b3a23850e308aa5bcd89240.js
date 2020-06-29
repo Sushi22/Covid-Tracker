76e060aba1ea91dbe5836cd3cbae70e80448cb72
Sushi22 <sushmitajoshi22@gmail.com>
2020-06-29 13:48:11 +0530

    Updates

---
+++ precache-manifest.4c9584ae1b3a23850e308aa5bcd89240.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f17450c1afd122461a29d2ca5bc0d511",
+    "url": "/Covid-Tracker/index.html"
+  },
+  {
+    "revision": "d1cc4d2a2ee783a6b9b5",
+    "url": "/Covid-Tracker/static/css/main.e0359d07.chunk.css"
+  },
+  {
+    "revision": "91464b2c1486d28b6a15",
+    "url": "/Covid-Tracker/static/js/2.e0181ec3.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Covid-Tracker/static/js/2.e0181ec3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1cc4d2a2ee783a6b9b5",
+    "url": "/Covid-Tracker/static/js/main.d5b2220a.chunk.js"
+  },
+  {
+    "revision": "a2683faba10be9e4f7fb",
+    "url": "/Covid-Tracker/static/js/runtime-main.32f9c4ab.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/Covid-Tracker/static/media/image.d7265326.png"
+  }
+]);
